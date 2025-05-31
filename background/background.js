2fbb3cf49ce11a3cc6f72ed6199100292c572713
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-31 16:53:18 -0300

    feat: while there are tabs with the same url open, do not request authentication

--- background/background.js
+++ background/background.js
@@ -11,3 +11,14 @@ browser.runtime.onMessage.addListener((message, sender) => {
       console.log(`no-action-for-message: ${message.type}`);
   }
 })
+
+browser.tabs.onRemoved.addListener(async (tab_id, _) => {
+  for (const root of Array.from(authenticatedUrls)) {
+    let tabs = await browser.tabs.query({url: `${root}/*`});
+    tabs = tabs.filter(tab => tab.id !== tab_id);
+
+    if (tabs.length === 0) {
+      authenticatedUrls.delete(root);
+    }
+  }
+})
